d0fd0cb63e7cab9b4959ddf0e06126f09b3d696e
khoa bui <averageazn1337@gmail.com>
2013-06-06 11:38:06 -0500

    cleaned up more stuff

--- config/initializers/my_tasks_scheduler.rb
+++ config/initializers/my_tasks_scheduler.rb
@@ -7,7 +7,7 @@ require 'rubygems'   # Need this to make use of any Gem, in our case it is rufus
 require 'rufus/scheduler'  # Need this to make use of Rufus::Scheduler
 scheduler = Rufus::Scheduler.start_new
 today= Date.today
-scheduler.every '1m' do
+scheduler.every '15m' do
    
 
     
@@ -27,119 +27,120 @@ scheduler.every '1m' do
      end
     
       db.execute("select * from events where events.creator ='"+name+"'") do |event|
-        #puts "in event"
-        
-        event_name=event[1]
-        event_desc=event[2]
-        #24 last event
-        event_date=event[3]
-        event_time=event[26] 
-         
-         puts event_date
-        if Date.parse(event_date) < Date.today or holidays.include? event_date
-          puts "skip"
+          #puts "in event"
           
-        else
-         if event[9]
+          event_name=event[1]
+          event_desc=event[2]
+          #24 last event
+          event_date=event[3]
+          event_time=event[26] 
            
-          scheduler.cron '13 0 10 * * 1-5' do
-          
+           
 
+          if Date.parse(event_date) < Date.today or holidays.include? event_date
+            puts "skip"
             
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-         
-
-
+          else
+           if event[9]
+             
+            scheduler.cron '13 0 10 * * 1-5' do
+            
+  
+              
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+           
+  
+  
+             end
+          end
+          
+          if event[10]
+            puts "in 10"
+            scheduler.cron '0 10 * * 6' do
+           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver  
+          
+  
            end
-        end
-        
-        if event[10]
-          puts "in 10"
-          scheduler.cron '0 10 * * 6' do
-         UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver  
-        
-
-         end
-        end
-        if event[11]
-          scheduler.cron '0 10 * * 6-7' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end
-        end
-        if event[12]
-            scheduler.cron '0 10 * * 1-7' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end
-        end
-        if event[13]
-          scheduler.cron '0 10 * * 1-5' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end        
-        end
-        if event[14]
-          scheduler.cron '0 10 * * 1' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end        
-        end
-        if event[15]
-          #biweekly
-          scheduler.cron '0 10 * * 2,4' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end        
-        end
-        
-        if event[16]
-          #monthly
-          scheduler.cron '0 10 1 * *' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end        
-        end
-        if event[17]
-          #yearly
-          scheduler.cron '0 10 1 1 * *' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end
-        end
-        if event[18]
-          #semiyearly
-          scheduler.cron '0 10 1 1,7 * *' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end       
-        end
-        if event[19]
-          #quarterly
-          scheduler.cron '0 10 1 1,4,7,10 * *' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end        
-        end
-        if event[20]
-          scheduler.cron '0 10 * * 1' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end        
-        end
-        if event[21]
-          scheduler.cron '0 10 * * 2' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end        
-        end
-        if event[22]
-          scheduler.cron '0 10 * * 3' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end        
-        end
-        if event[23]
-          scheduler.cron '0 10 * * 4' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end        
-        end
-        if event[24]
-          scheduler.cron '0 10 * * 5' do
-           UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
-        end   
-         end
+          end
+          if event[11]
+            scheduler.cron '0 10 * * 6-7' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end
+          end
+          if event[12]
+              scheduler.cron '0 10 * * 1-7' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end
+          end
+          if event[13]
+            scheduler.cron '0 10 * * 1-5' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end        
+          end
+          if event[14]
+            scheduler.cron '0 10 * * 1' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end        
+          end
+          if event[15]
+            #biweekly
+            scheduler.cron '0 10 * * 2,4' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end        
+          end
+          
+          if event[16]
+            #monthly
+            scheduler.cron '0 10 1 * *' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end        
+          end
+          if event[17]
+            #yearly
+            scheduler.cron '0 10 1 1 * *' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end
+          end
+          if event[18]
+            #semiyearly
+            scheduler.cron '0 10 1 1,7 * *' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end       
+          end
+          if event[19]
+            #quarterly
+            scheduler.cron '0 10 1 1,4,7,10 * *' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end        
+          end
+          if event[20]
+            scheduler.cron '0 10 * * 1' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end        
+          end
+          if event[21]
+            scheduler.cron '0 10 * * 2' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end        
+          end
+          if event[22]
+            scheduler.cron '0 10 * * 3' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end        
+          end
+          if event[23]
+            scheduler.cron '0 10 * * 4' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end        
+          end
+          if event[24]
+            scheduler.cron '0 10 * * 5' do
+             UserMailer.event(name,email,event_name,event_desc,event_date,event_time).deliver 
+          end   
+          end
          
           
-            
+         holidays.clear   
         end
           
 
